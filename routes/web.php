5a9a222e2e0e494edfaf3ac6c0eeb2119a1cdd9e
Edenilson Ruiz <ruiz.edenilson@gmail.com>
2024-06-03 05:01:57 -0600

    upd: panel behavior

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Livewire\ListPosts;
-use Illuminate\Support\Facades\Route;
+//use App\Livewire\ListPosts;
+//use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,8 +17,8 @@ use Illuminate\Support\Facades\Route;
 //Route::get('/', function () {
 //    return view('welcome');
 //});
-
-
-Route::get('/', ListPosts::class);
-
-Route::get('products', ListPosts::class);
+//
+//
+//Route::get('/', ListPosts::class);
+//
+//Route::get('products', ListPosts::class);
